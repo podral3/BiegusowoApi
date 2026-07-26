9fe787b82a544fed2034e6d079d6d2fc1d13ba2e
podral3 <podral3@gmail.com>
2026-07-26 20:04:52 +0200

    easy static data controller

--- BiegusowoApi/Controllers/DataController.cs
+++ BiegusowoApi/Controllers/DataController.cs
@@ -2,6 +2,7 @@
 using BiegusowoApi.Domain.Dtos.Misc;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BiegusowoApi.Controllers;
 
@@ -15,6 +16,14 @@ public class DataController(ApplicationDbContext dbContext) : ControllerBase
     [ProducesResponseType(typeof(LookupDataDto), StatusCodes.Status200OK)]
     public async Task<ActionResult<LookupDataDto>> GetLookups()
     {
-        throw new NotImplementedException();    
+        var breeds = (await _dbContext.Breeds.ToListAsync())
+            .Select(b => new BreedDto(b))
+            .ToList();
+        var species = (await _dbContext.Species.ToListAsync())
+            .Select(s => new SpeciesDto(s))
+            .ToList();
+        var voivodeships = await _dbContext.Voivodeships.ToListAsync();
+
+        return Ok(new LookupDataDto(species, breeds, voivodeships));
     }
 }
