91abf2df5f2b012a1699370cc5842cc7747ca2ba
podral3 <podral3@gmail.com>
2026-08-15 15:05:50 +0200

    cors for dev

--- BiegusowoApi/Shared/Helpers/Composition/ConfigureCors.cs
+++ BiegusowoApi/Shared/Helpers/Composition/ConfigureCors.cs
@@ -2,13 +2,31 @@
 
 public static class ConfigureCors
 {
-    public static IServiceCollection ConfigureCorsOrigins(this IServiceCollection services)
+    public static IServiceCollection ConfigureCorsOrigins(
+    this IServiceCollection services,
+    IConfiguration configuration)
     {
         services.AddCors(options =>
+        {
             options.AddPolicy("AllowFrontend", policy =>
-                policy.WithOrigins("https://mapa-drobiu.pl")
-                    .AllowAnyMethod()
-                    .AllowAnyHeader()));
+            {
+                if (configuration.GetValue<bool>("IsDevelopment"))
+                {
+                    policy
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                }
+                else
+                {
+                    policy
+                        .WithOrigins("https://mapa-drobiu.pl")
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                }
+            });
+        });
+
         return services;
     }
 }
