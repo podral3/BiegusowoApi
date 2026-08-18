294fe115e1d164d5cb9e49ad9c1febed0ecf6afa
podral3 <podral3@gmail.com>
2026-08-18 12:30:14 +0200

    move dockerfile to root, add github action to build and push image to registry

--- BiegusowoApi.AppHost/AppHost.cs
+++ BiegusowoApi.AppHost/AppHost.cs
@@ -1,27 +1,56 @@
+using Microsoft.Extensions.Configuration;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddDockerComposeEnvironment("env");
+builder.AddDockerComposeEnvironment("env")
+        .WithDashboard(false); //for staging envs etc it should be on, needs modifications of appsettings
 
-var databaseContextPath = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "../../../../Database")); //bruh 
+var api = builder.AddProject<Projects.BiegusowoApi>("biegusowoapi");
 
-var pgUserName = builder.AddParameter("postgres-username", "postgres");
-var pgPassword = builder.AddParameter("postgres-password", "postgres", secret: true);
-var postgres = builder.AddPostgres("postgres",
-    userName: pgUserName,
-    password: pgPassword,
-    port: 5432)
+var useLocalPostgres =
+    builder.Configuration.GetValue("UseLocalPostgres", true);
 
-    .WithDockerfile(databaseContextPath)
-    .WithDataVolume()
-    .WithPgAdmin(pgadmin => pgadmin.WithHostPort(2137));
+if (useLocalPostgres)
+{
+    var databaseContextPath = Path.GetFullPath(
+        Path.Combine(AppContext.BaseDirectory, "../../../../Database"));
 
-var appDb = postgres.AddDatabase("biegusowo");
+    var pgUserName = builder.AddParameter(
+        "postgres-username",
+        "postgres");
 
-var s3SecretKey = builder.AddParameter("s3-secret-key", secret: true);
+    var pgPassword = builder.AddParameter(
+        "postgres-password",
+        "postgres",
+        secret: true);
 
-builder.AddProject<Projects.BiegusowoApi>("biegusowoapi")
-    .WithReference(appDb)
-    .WaitFor(appDb)
-    .WithEnvironment("S3__SecretKey", s3SecretKey);
+    var postgres = builder.AddPostgres(
+            "postgres",
+            userName: pgUserName,
+            password: pgPassword,
+            port: 5432)
+        .WithDockerfile(databaseContextPath)
+        .WithDataVolume()
+        .WithPgAdmin(pgadmin =>
+            pgadmin.WithHostPort(2137));
+
+    var appDb = postgres.AddDatabase("biegusowo");
+
+    api
+        .WithReference(appDb)
+        .WaitFor(appDb);
+}
+else
+{
+    var appDb = builder.AddConnectionString("DatabaseConnectionString",
+        environmentVariableName: "Database__ConnectionString");
+
+    api.WithReference(appDb);
+}
+
+var s3SecretKey =
+    builder.AddParameter("s3-secret-key", secret: true);
+
+api.WithEnvironment("S3__SecretKey", s3SecretKey);
 
 builder.Build().Run();
